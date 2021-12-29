7528fb89cc098880865660810b1b74aab6c3f3fc
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2021-12-29 02:25:56 +0100

    Fix: Weirdness

--- src/util/functions.py
+++ src/util/functions.py
@@ -3,11 +3,11 @@ from nextcord.ext import commands
 
 
 def format_time(_time: datetime.time) -> str:
-    return _time.strftime('%H:%M:%S')
+    return _time.strftime("%H:%M:%S")
 
 
 def format_date(_date: datetime.date) -> str:
-    return _date.strftime('%d.%m.%Y')
+    return _date.strftime("%d.%m.%Y")
 
 
 def format_to_db_date(_date: datetime) -> str:
@@ -15,10 +15,10 @@ def format_to_db_date(_date: datetime) -> str:
 
 
 def format_date_time(_datetime: datetime) -> str:
-    return _datetime.strftime('%H:%M:%S, %d.%m.%Y')
+    return _datetime.strftime("%H:%M:%S, %d.%m.%Y")
 
 
-def list_options(command: commands.Group) -> str:
-    options = [f"`!!{cmd.qualified_name}`" for cmd in commands.Group.walk_commands(command)]
+def list_options(group: commands.Group) -> str:
+    options = [f"`!!{cmd.qualified_name}`" for cmd in group.walk_commands()]
     params = ", ".join(options)
     return params
