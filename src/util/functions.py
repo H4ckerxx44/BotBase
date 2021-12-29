fff9463c47eb4883970449f420e7e3b35468ba81
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2021-12-29 02:03:30 +0100

    Fix: Reindent with spaces

--- src/util/functions.py
+++ src/util/functions.py
@@ -3,22 +3,22 @@ from nextcord.ext import commands
 
 
 def format_time(_time: datetime.time) -> str:
-	return _time.strftime('%H:%M:%S')
+    return _time.strftime('%H:%M:%S')
 
 
 def format_date(_date: datetime.date) -> str:
-	return _date.strftime('%d.%m.%Y')
+    return _date.strftime('%d.%m.%Y')
 
 
 def format_to_db_date(_date: datetime) -> str:
-	return _date.strftime("%Y-%m-%d %H:%M:%S")
+    return _date.strftime("%Y-%m-%d %H:%M:%S")
 
 
 def format_date_time(_datetime: datetime) -> str:
-	return _datetime.strftime('%H:%M:%S, %d.%m.%Y')
+    return _datetime.strftime('%H:%M:%S, %d.%m.%Y')
 
 
 def list_options(command: commands.Group) -> str:
-	options = [f"`!!{cmd.qualified_name}`" for cmd in commands.Group.walk_commands(command)]
-	params = ", ".join(options)
-	return params
+    options = [f"`!!{cmd.qualified_name}`" for cmd in commands.Group.walk_commands(command)]
+    params = ", ".join(options)
+    return params
