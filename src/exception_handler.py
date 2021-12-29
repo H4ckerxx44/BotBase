72728aaefd633a500c4db4ddbda0aa377b5e4c13
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2021-12-29 04:13:46 +0100

    Styles: Black formatting

--- src/exception_handler.py
+++ src/exception_handler.py
@@ -37,21 +37,24 @@ class ExceptionHandler(commands.Cog):
                 title="Missing parameter!",
                 description=f"You are missing `{err.param.name}`.\nUsage: `{ctx.clean_prefix}{ctx.command.qualified_name} {ctx.command.signature}`",
                 color=self.error_color,
-                timestamp=datetime.datetime.now())
+                timestamp=datetime.datetime.now(),
+            )
 
         elif isinstance(err, commands.NotOwner):
             emb = nextcord.Embed(
                 title="Not Owner!",
                 description=f"Only the bot-owner can execute self command.",
                 color=self.error_color,
-                timestamp=datetime.datetime.now())
+                timestamp=datetime.datetime.now(),
+            )
 
         elif isinstance(err, commands.MemberNotFound):
             emb = nextcord.Embed(
                 title="Member not found!",
                 description=f"The given member ({err.argument}) could not be found.",
                 color=self.error_color,
-                timestamp=datetime.datetime.now())
+                timestamp=datetime.datetime.now(),
+            )
 
         elif isinstance(err, commands.MissingRole):
             role = ctx.guild.get_role(err.missing_role)
@@ -67,7 +70,7 @@ class ExceptionHandler(commands.Cog):
                 title="Guild not found!",
                 description=f"The given guild ({err.argument}) could not be found.",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.TooManyArguments):
@@ -75,7 +78,7 @@ class ExceptionHandler(commands.Cog):
                 title="Too many argument!",
                 description=f"You supplied too many arguments. ({err.args})",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.RoleNotFound):
@@ -83,7 +86,7 @@ class ExceptionHandler(commands.Cog):
                 title="Role not found!",
                 description=f"The given role ({err.argument}) could not be found.",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.MessageNotFound):
@@ -91,7 +94,7 @@ class ExceptionHandler(commands.Cog):
                 title="Message not found!",
                 description=f"The given message ({err.argument}) could not be found.",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.ChannelNotFound):
@@ -99,7 +102,7 @@ class ExceptionHandler(commands.Cog):
                 title="Channel not found!",
                 description=f"The given channel ({err.argument}) could not be found.",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.UnexpectedQuoteError):
@@ -108,23 +111,23 @@ class ExceptionHandler(commands.Cog):
                 title="",
                 description="",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.InvalidEndOfQuotedStringError):
             emb = nextcord.Embed(
                 title="Invalid end of quoted string!",
-                description="The quoted string you have provided is invalid. Perhaps you got \" and/or \' mixed up?",
+                description="The quoted string you have provided is invalid. Perhaps you got \" and/or ' mixed up?",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.ExpectedClosingQuoteError):
             emb = nextcord.Embed(
                 title="Expected closing quote!",
-                description="The quoted string you have provided is invalid. Perhaps you forgot to end the string with \" or \'",
+                description="The quoted string you have provided is invalid. Perhaps you forgot to end the string with \" or '",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.PrivateMessageOnly):
@@ -132,7 +135,7 @@ class ExceptionHandler(commands.Cog):
                 title="Private message only!",
                 description="This command only works in private messages.",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.NoPrivateMessage):
@@ -140,7 +143,7 @@ class ExceptionHandler(commands.Cog):
                 title="No private message!",
                 description="This command only works on a guild.",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.MissingPermissions):
@@ -148,7 +151,7 @@ class ExceptionHandler(commands.Cog):
                 title="Missing permissions!",
                 description=f"You are missing {', '.join(f'`{e}`' for e in err.missing_permissions)}",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.BotMissingPermissions):
@@ -156,27 +159,27 @@ class ExceptionHandler(commands.Cog):
                 title="Bot missing permissions!",
                 description=f"The bot is missing {', '.join(f'`{e}`' for e in err.missing_permissions)}",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.MissingAnyRole):
-            roles = ', '.join([x for x in err.missing_roles])
+            roles = ", ".join([x for x in err.missing_roles])
             emb = nextcord.Embed(
                 title="Missing any role!",
                 description=f"You are missing one of the following roles ({roles})",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.BotMissingAnyRole):
-            missing_roles = '{}, or {}'.format(", ".join(err.missing_roles[:-1]),
-                                               err.missing_roles[-1])
+            missing_roles = "{}, or {}".format(
+                ", ".join(err.missing_roles[:-1]), err.missing_roles[-1]
+            )
             emb = nextcord.Embed(
                 title="Bot missing role!",
                 description=f"The bot is missing {missing_roles}",
-
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.BotMissingRole):
@@ -185,7 +188,7 @@ class ExceptionHandler(commands.Cog):
                 title="Bot missing role!",
                 description=f"The bot is missing {role.mention}",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.NSFWChannelRequired):
@@ -209,7 +212,7 @@ class ExceptionHandler(commands.Cog):
                 title="Command on cooldown!",
                 description=f"This command is on cooldown, retry after {err.retry_after} seconds.",
                 color=self.error_color,
-                timestamp=datetime.datetime.now()
+                timestamp=datetime.datetime.now(),
             )
 
         elif isinstance(err, commands.ExtensionAlreadyLoaded):
@@ -278,26 +281,27 @@ class ExceptionHandler(commands.Cog):
             self.client.log_command_stats(str(ctx.command.qualified_name), "ERROR")
         await ctx.send(embed=emb)
 
-
     def gen_default_embed(self, exception):
         else_error = nextcord.Embed(
             title="Unhandled exception occurred!",
             description=str(exception.__class__.__name__),
             color=self.error_color,
-            timestamp=datetime.datetime.now()
+            timestamp=datetime.datetime.now(),
         )
         else_error.set_footer(text="My Developer has been notified")
         return else_error
 
-
     def gen_dennis_embed(self, exception) -> nextcord.Embed:
-        exc = "".join(traceback.format_exception(etype=type(exception), value=exception,
-                                                 tb=exception.__traceback__))
+        exc = "".join(
+            traceback.format_exception(
+                etype=type(exception), value=exception, tb=exception.__traceback__
+            )
+        )
         dennis_error_embed = nextcord.Embed(
             title="Unhandled exception occurred!",
             description=f"```py\n{exc}```",
             color=self.error_color,
-            timestamp=datetime.datetime.now()
+            timestamp=datetime.datetime.now(),
         )
         return dennis_error_embed
 
