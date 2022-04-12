c4599098f27c6da7225f27413141b30f8fc5a9c1
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2022-04-12 13:56:30 +0200

    Style[slashcommands]: Formatting

--- src/cogs/slashcommands.py
+++ src/cogs/slashcommands.py
@@ -19,7 +19,9 @@ class SlashCommands(commands.Cog):
     def __init__(self, bot: CustomBot):
         self.bot: CustomBot = bot
 
-    @nextcord.slash_command(guild_ids=TESTING_GUILD_IDS, description="Simple slash command.")
+    @nextcord.slash_command(
+        guild_ids=TESTING_GUILD_IDS, description="Simple slash command."
+    )
     async def basic_slash(self, interaction: nextcord.Interaction) -> nextcord.Message:
         """
         Template command with text response
@@ -30,24 +32,36 @@ class SlashCommands(commands.Cog):
 
     @nextcord.slash_command(guild_ids=TESTING_GUILD_IDS, description="Test command")
     async def my_slash_command(self, interaction: nextcord.Interaction):
-        return await interaction.response.send_message("This is a slash command in a cog!")
+        return await interaction.response.send_message(
+            "This is a slash command in a cog!"
+        )
 
     @nextcord.slash_command(guild_ids=TESTING_GUILD_IDS)
-    async def my_user_command(self, interaction: nextcord.Interaction, member: nextcord.Member):
+    async def my_user_command(
+        self, interaction: nextcord.Interaction, member: nextcord.Member
+    ):
         return await interaction.response.send_message(f"Hello, {member}!")
 
     @nextcord.slash_command(guild_ids=TESTING_GUILD_IDS, description="Idk.")
-    async def my_message_command(self, interaction: nextcord.Interaction, message: nextcord.Message):
+    async def my_message_command(
+        self, interaction: nextcord.Interaction, message: nextcord.Message
+    ):
         return await interaction.response.send_message(f"{message}")
 
     @nextcord.slash_command(guild_ids=TESTING_GUILD_IDS, description="Bot info.")
     async def bot_info(self, interaction: nextcord.Interaction):
-        e = nextcord.Embed(title=self.bot.user.display_name,
-                           description=f"Version {'.'.join(self.bot.version)}")
+        e = nextcord.Embed(
+            title=self.bot.user.display_name,
+            description=f"Version {'.'.join(self.bot.version)}",
+        )
         return await interaction.response.send_message(embed=e)
 
     @nextcord.slash_command(guild_ids=TESTING_GUILD_IDS, description="This is a test.")
-    async def alexlol(self, interaction: nextcord.Interaction, arg: str = nextcord.SlashOption(description="Hallo.")):
+    async def alexlol(
+        self,
+        interaction: nextcord.Interaction,
+        arg: str = nextcord.SlashOption(description="Hallo."),
+    ):
         return await interaction.response.send_message(f"The arg is {arg}.")
 
 
